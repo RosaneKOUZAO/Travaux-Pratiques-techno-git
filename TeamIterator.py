2982c43f306ba58e58fdf02dd56587fed5dfd8e9
Aboubacar camara <aboubacre14207@gmail.com>
2026-02-04 15:14:27 +0100

    implementation de la classe teamiterator issue10

---
+++ TeamIterator.py
@@ -0,0 +1,12 @@
+class TeamIterator:
+    def __init__(self, team):
+        self.__team = team
+        self.__index = 0
+
+    def __next__(self):
+        if self.__index >= len(self.__team):
+            raise StopIteration
+
+        element = self.__team[self.__index]
+        self.__index += 1
+        return element
