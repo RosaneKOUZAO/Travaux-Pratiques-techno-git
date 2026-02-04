94ed314cd66d62ca5d088ac49bb8f33fddc36173
hasalpcode <hasalpcode784@gmail.com>
2026-02-04 13:14:13 +0000

    issue#1 add class warrior

---
+++ Warrion.py
@@ -0,0 +1,13 @@
+import random
+class Warrior:
+    degat = random.randint(3, 5)
+    chance = 5
+    fuite = 3
+    prix = 10
+    type_unite = "warrior"
+    def __init__(self):
+        self.degat = self.degat
+        self.chance = self.chance
+        self.fuite = self.fuite
+        self.prix = self.prix
+        self.type_unite = self.type_unite
